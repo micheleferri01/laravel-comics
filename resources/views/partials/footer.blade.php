bec67137bd8c1e8d67092a94ac1b00a7a7fc59ed
micheleferri01 <micheleferri095@gmail.com>
2026-05-19 23:19:42 +0200

    add: footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,40 @@
+<footer class='position-relative'>
+    <div class="bg-blue text-white py-4 position-relative">
+        <section class="container py-3" id="shops">
+            <ul class='shops-list m-0'>
+                <li><a href="#"><img src="{{Vite::asset('resources/img/buy-comics-digital-comics.png')}}" alt="Digital comics logo" class='shop-icons' />  DIGITAL COMICS</a></li>
+                <li><a href="#"><img src="{{Vite::asset('resources/img/buy-comics-merchandise.png')}}" alt="Dc merchandise logo" class='shop-icons' />  DC MERCHANDISE</a></li>
+                <li><a href="#"><img src="{{Vite::asset('resources/img/buy-comics-subscriptions.png')}}" alt="subscription logo" class='shop-icons' />  SUBSCRIPTION</a></li>
+                <li><a href="#"><img src="{{Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="Comic shop locator logo" class='shop-icons' />  COMIC SHOP LOCATOR</a></li>
+                <li><a href="#"><img src="{{Vite::asset('resources/img/buy-dc-power-visa.svg')}}" alt="Dc power visa logo" class='shop-icons' />  DC POWER VISA</a></li>
+            </ul>
+        </section>
+    </div>
+    <div class='bg-footer-links text-white'>
+        <div class="container d-flex justify-content-between align-items-center">
+            <div class='pt-4 pb-2 footer-links-container'>
+
+                
+
+            </div>
+            <figure class='logo-footer-container m-0'>
+                <img src="{{Vite::asset('resources/img/dc-logo-bg.png')}}" alt="dc-logo"  class='logo-footer'/>
+            </figure>
+        </div>
+    </div>
+    <div class='bg-dark-custom py-5 newsletter-and-socials'>
+        <div class='container d-flex justify-content-between align-items-center'>
+            <div>
+                <a href="#" class='text-white signup-btn'>SIGN-UP NOW!</a>
+            </div>
+            <div class='socials'>
+                <h2 class='fw-bold fs-4 m-0'>FOLLOW US</h2>
+                <a href="#"><img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt="facebook" class='social' /></a>
+                <a href="#"><img src="{{Vite::asset('resources/img/footer-twitter.png')}}" alt="twitter" class='social' /></a>
+                <a href="#"><img src="{{Vite::asset('resources/img/footer-youtube.png')}}" alt="you tube" class='social' /></a>
+                <a href="#"><img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt="pinterest" class='social' /></a>
+                <a href="#"><img src="{{Vite::asset('resources/img/footer-periscope.png')}}" alt="periscope" class='social' /></a>
+            </div>
+        </div>
+    </div>
+</footer>
