784877d9f62f979ce86ea8f32f0c260a96f21e41
micheleferri01 <micheleferri095@gmail.com>
2026-05-20 18:54:20 +0200

    add: config footermenus

---
+++ config/footermenus.php
@@ -0,0 +1,125 @@
+<?php
+
+return [
+    [
+      "title"=> 'DC COMICS',
+      "links"=> [
+        [
+          "url"=> '#',
+          "text"=> 'Characters'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'Comics'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'Movies'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'TV'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'Games'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'Videos'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'News'
+        ],
+      ]
+    ],
+    [
+      "title"=> 'SHOP',
+      "links"=> [
+        [
+          "url"=> '#',
+          "text"=> 'Shop DC'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'Shop Dc Collectibles'
+        ]
+
+      ]
+    ],
+    [
+      "title"=> 'DC',
+      "links"=> [
+        [
+          "url"=> '#',
+          "text"=> 'Terms Of Use'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'Privacy policy (New)'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'Ad Choices'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'Advertising'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'Jobs'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'Subscriptions'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'Talent Workshops'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'CPSC Certificates'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'Ratings'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'Shop Help'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'Contact Us'
+        ]
+      ]
+    ],
+    [
+      "title"=> 'SITES',
+      "links"=> [
+        [
+          "url"=> '#',
+          "text"=> 'DC'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'MAD Magazine'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'DC Kids'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'DC Universe'
+        ],
+        [
+          "url"=> '#',
+          "text"=> 'DC Power Visa'
+        ]
+      ]
+    ]
+  ];
