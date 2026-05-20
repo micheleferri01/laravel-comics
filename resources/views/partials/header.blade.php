dd186033de761151fd76e087874f8bfbe220fed6
micheleferri01 <micheleferri095@gmail.com>
2026-05-20 19:01:14 +0200

    add:header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,22 @@
+<header class="bg-white">
+    <div class="container">
+        <nav class='container-fluid'>
+            <figure class='logo-header'>
+                <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="Logo" class='img-fluid' />
+            </figure>
+            <ul class="d-flex gap-3">
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+        </nav>
+        
+    </div>
+</header>
