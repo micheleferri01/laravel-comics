f2cf4306032d5ff774d62cc57aada9680ac9520a
micheleferri01 <micheleferri095@gmail.com>
2026-05-20 17:43:39 +0200

    add: main section of home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DC comics</title>
-</head>
-<body>
-    
-</body>
-</html>
+
+ @extends('layouts/master')
+
+ @section('main-section')
+
+ @php
+    $comics = config('comics');
+ @endphp
+    <main>
+        <div class='ow-hidden jumbo'>
+            <img src="{{Vite::asset('resources/img/jumbotron.jpg')}}" alt="jumbotron" />
+        </div>
+        <div class="bg-comics text-white pt-5 pb-3 position-relative">
+            <div class='container position-relative'>
+                <h1 class='bg-blue fs-3 fw-bold px-3 py-1 my-badge'>CURRENT SERIES</h1>
+                <div class='row g-3'>
+                    @foreach($comics as $comic)
+                        <x-card>
+                            <x-slot:thumb>{{$comic['thumb']}}</x-slot>
+                            <x-slot:title>{{$comic['title']}}</x-slot>
+                            <x-slot:series>{{$comic['series']}}</x-slot>
+                        </x-card>
+                    @endforeach
+                </div>
+                <button class='bg-blue text-white py-2 px-5 btn-load-more'>LOAD MORE</button>
+            </div>
+        </div>
+
+    </main>
+ @endsection
