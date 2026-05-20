c4096a5e155975cb309f215dcec9159651c93269
micheleferri01 <micheleferri095@gmail.com>
2026-05-20 17:42:00 +0200

    add: component card

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,8 @@
+<div class="col-6 col-md-4 col-lg-3 col-xl-2">
+    <div class='h-100 p-2'>
+        <div class='card-image'>
+            <img src={{$thumb}} alt={{$title}} class='img-fluid' />
+        </div>
+        <h1 class='fs-6 pt-2'>{{$series}}</h1>
+    </div>
+</div>
